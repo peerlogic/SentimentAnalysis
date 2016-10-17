89a7cb060c57712449f9d6c78eaee2c94c60e55e
ferry <ferryxo@yahoo.com>
2016-10-17 18:38:16 -0400

    map sentiment score to rgb (need to check the mapping)

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,13 +1,52 @@
 from flask import Flask, render_template, request, jsonify
+from flask_api import status
 from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
 from nltk.tokenize import sent_tokenize
+import colorsys
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
+def get_rgb_from_hue_spectrum(value, start_hue, end_hue):
+    value = round((value + 1) * 255/2 - 1)
+    # spectrum is red (0.0), orange, yellow, green, blue, indigo, violet (0.9)
+    # hue = percent * (end_hue - start_hue) + start_hue
+    # lightness = 0.5
+    # saturation = 1
+    # r, g, b = colorsys.hls_to_rgb(hue, lightness, saturation)
+    r = int(255 - value)
+    g = int(value)
+    b = 0
+
+    return "rgb(" + str(r) + "," + str(g) + "," + str(b) + ")"
+
+@app.route('/developer', methods=['GET'])
+def developer():
+    return render_template("developer.html")
+
+@app.route('/instructor', methods=['GET'])
+def instructor():
+    return render_template("instructor.html")
+
+@app.route('/visualize_sentiment', methods=['GET', 'POST'])
+def visualize_sentiment():
+    if request.method == 'GET':
+        review = request.args.get('review')
+    elif request.method == 'POST':
+        review = request.json['review']
+
+    if review != '':
+        sentences = sent_tokenize(review)
+
+        overall_compound = 0.0;
+
+        for sentence in sentences:
+            sa = vaderSentiment(sentence)
+            review = review.replace(sentence, '<font color="' + get_rgb_from_hue_spectrum(float(sa['compound']), 0, 0.3) + '">' + sentence + '[' + str(sa['compound']) + ']</font>')
+            overall_compound += float(sa['compound'])
+
+        return review, status.HTTP_200_OK
+
 
 @app.route('/analyze_review', methods=['GET', 'POST'])
 def analyze_review():
@@ -29,7 +68,7 @@ def analyze_review():
             results[sentence] = sa
             overall_compound += float(sa['compound'])
 
-        return jsonify(overall_compound = overall_compound, sentiments=results)
+        return jsonify(overall_compound = overall_compound, sentiments=results), status.HTTP_200_OK
 
 @app.route('/analyze_sentences', methods=['GET', 'POST'])
 def analyze_sentences():
@@ -49,7 +88,7 @@ def analyze_sentences():
             results[sentence] = sa
             overall_compound += float(sa['compound'])
 
-        return jsonify(overall_compound = overall_compound, sentiments=results)
+        return jsonify(overall_compound = overall_compound, sentiments=results), status.HTTP_200_OK
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3008, threaded=True)
